fea030a64c0061da64f3f151dbfdc29cb0c63ebd
zetashift <rskaraya@gmail.com>
2021-04-10 23:14:06 +0200

    More formatting testing

--- .config/nvim/lua/settings/compe.lua
+++ .config/nvim/lua/settings/compe.lua
@@ -2,34 +2,34 @@ local M = {}
 
 -- Compe setup from @ckipp01
 M.setup = function()
-	local langs = {
-		"scala",
-		"html",
-		"javascript",
-		"lua",
-		"java",
-		"nim",
-		"elixir",
-		"nix",
-		"javascriptreact",
-		"typescript",
-	}
-	require("compe").setup({
-		enabled = true,
-		debug = false,
-		min_length = 3,
+  local langs = {
+    "scala",
+    "html",
+    "javascript",
+    "lua",
+    "java",
+    "nim",
+    "elixir",
+    "nix",
+    "javascriptreact",
+    "typescript",
+  }
+  require("compe").setup({
+    enabled = true,
+    debug = false,
+    min_length = 3,
 
-		source = {
-			path = true,
-			buffer = true,
-			vsnip = true,
-			nvim_lsp = {
-				priority = 1000,
-				filetypes = langs,
-			},
-		},
-	})
-	-- set_keybinds()
+    source = {
+      path = true,
+      buffer = true,
+      vsnip = true,
+      nvim_lsp = {
+        priority = 1000,
+        filetypes = langs,
+      },
+    },
+  })
+  -- set_keybinds()
 end
 
 return M
