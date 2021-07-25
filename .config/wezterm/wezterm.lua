c939a3865249321f6a7b2771bf450c78aecaefce
zetashift <rskaraya@gmail.com>
2021-07-25 21:11:32 +0200

    Update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,20 @@
+
 local wezterm = require 'wezterm'
 
 return {
-    font = wezterm.font("JetBrains Mono"),
-    font_size = 15,
+	  font = wezterm.font("Iosevka"),
+    font_size = 18,
     color_scheme = "Gruvbox Dark",
-    window_background_opacity = 0.95,
-}
+		hide_tab_bar_if_only_one_tab = true,
+		text_background_opacity = 1.0,
+		window_background_image = "/home/rishi/.config/wezterm/wall.jpg",
+		window_background_image_hsb = {
+			brightness = 0.38,
+			hue = 1.0,
+			saturation = 0.6
+		},
+		keys = {
+			{ key="[", mods="SUPER", action=wezterm.action{ActivateTabRelative=-1} },
+			{ key="]", mods="SUPER", action=wezterm.action{ActivateTabRelative=1} }
+		}
+	}
