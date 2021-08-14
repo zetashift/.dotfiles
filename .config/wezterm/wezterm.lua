9623297bd40dd559c82dd68495b7cb7e2f876c43
zetashift <rskaraya@gmail.com>
2021-08-14 19:41:44 +0200

    Small wezterm update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,35 +1,34 @@
-
 local wezterm = require 'wezterm'
 
 return {
-	  font = wezterm.font_with_fallback({"Iosevka", "JetBrains Mono"}),
-    font_size = 18,
-    color_scheme = "Gruvbox Dark",
-		hide_tab_bar_if_only_one_tab = true,
-		window_background_opacity = 0.86,
-		colors = {
-			tab_bar = {
-				background = "#3c3836",
-				active_tab = {
-					bg_color = "#665c54",
-					fg_color = "#EDF5FD"
-				},
-				inactive_tab = {
-					bg_color = "#504945",
-					fg_color = "#928374"
-				},
-				inactive_tab_hover = {
-					bg_color = "#665c54",
-					fg_color = "#EDF5FD"
-				},
-				new_tab = {
-					bg_color = "#504945",
-					fg_color = "#928374"
-				},
-				new_tab_hover = {
-					bg_color = "#665c54",
-					fg_color = "#EDF5FD"
-				},
-			}
+	font = wezterm.font("Iosevka Term SS08", {weight="Light"}),
+	font_size = 16,
+	color_scheme = "Gruvbox Dark",
+	hide_tab_bar_if_only_one_tab = true,
+	window_background_opacity = 0.86,
+	colors = {
+		tab_bar = {
+			background = "#3c3836",
+			active_tab = {
+				bg_color = "#665c54",
+				fg_color = "#EDF5FD"
+			},
+			inactive_tab = {
+				bg_color = "#504945",
+				fg_color = "#928374"
+			},
+			inactive_tab_hover = {
+				bg_color = "#665c54",
+				fg_color = "#EDF5FD"
+			},
+			new_tab = {
+				bg_color = "#504945",
+				fg_color = "#928374"
+			},
+			new_tab_hover = {
+				bg_color = "#665c54",
+				fg_color = "#EDF5FD"
+			},
 		}
 	}
+}
