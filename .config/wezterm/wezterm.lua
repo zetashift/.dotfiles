0575c1a835af95161af55ee07653201c6ca9f1b0
zetashift <rskaraya@gmail.com>
2021-12-28 18:57:53 +0100

    Wezterm update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,9 +4,22 @@ return {
 	font = wezterm.font("Iosevka Term SS08", {weight="Light"}),
 	font_size = 16,
 	color_scheme = "Gruvbox Dark",
+        color_schemes = {
+          ["Gruvbox Dark"] = {
+            background = "#2b3339",
+          }
+        },
 	hide_tab_bar_if_only_one_tab = true,
-	window_background_opacity = 0.86,
+        window_padding = {
+          left = 0,
+          right = 0,
+          top = 0,
+          bottom = 0,
+        },
+	text_background_opacity = 0.86,
+	window_background_opacity = 0.88,
 	colors = {
+ 
 		tab_bar = {
 			background = "#3c3836",
 			active_tab = {
